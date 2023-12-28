d2bfc62293745bd94149489fc2cc5172094c7b1a
Ramill Ibragimov <ribragimov@malwarebytes.com>
2023-12-28 19:21:32 +0200

    Initial Commit

---
+++ TopDestinationApp/TopDestinationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TopDestinationAppApp.swift
+//  TopDestinationApp
+//
+//  Created by test on 28.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct TopDestinationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
